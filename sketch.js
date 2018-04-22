97d5be111c1ce7c0b9d72fcc62f7d7b802cb043e
artem108 <artem.arshaniy108@gmail.com>
2018-04-22 11:56:06 +0300

    Create project

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let angle = 0
+let w = 35
+let ma
+let maxD
+
+function setup() {
+  createCanvas(500, 500, WEBGL)
+  ma = atan(1 / sqrt(2))
+  maxD = dist(0, 0, 200, 200)
+}
+
+function draw() {
+
+  background(200)
+  ortho(-400, 400, -400, 400, 0, 1000)
+
+  // translate(0, 0, 100)
+
+  rotateX(-QUARTER_PI)
+  rotateY(ma)
+
+  let offset = 0;
+  for (let z = 0; z < height; z += w) {
+    for (let x = 0; x < width; x += w) {
+      push()
+      let d = dist(x, z, width / 2, height / 2)
+      let offset = map(d, 0, maxD, -PI, PI)
+      let a = angle + offset
+      let h = floor(map(sin(a), -1, 1, 50, 300))
+
+      translate(x - width / 2, 0, z - height / 2)
+      normalMaterial()
+      box(w - 2, h, w - 2)
+      // rect(x - width / 2 + w / 2, 0, w - 2, h)
+      pop()
+  }
+  // offset += 0.1
+}
+  angle -= 0.08
+}
